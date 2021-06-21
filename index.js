344033658c6708f666bf9e44be4a3207b655edc5
Pia Forastiere <pi.forastiere@gmail.comm>
2021-06-21 05:22:49 +0200

    fixing bugs

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ app.use('/api', routes())
 
 const port = process.env.PORT || 8080
 
-app.listen(port, () => {
-    console.log('Server is running!', PORT);
-});
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
